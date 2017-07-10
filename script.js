ba1f5a9bbe0ae2e2d124ab25d619797b78b890b7
Max <max417rosenberg@gmail.com>
2017-07-10 09:59:16 -0700

    Got rid of white borders, added 7 more colors, and changed measurements of color boxes

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-var paragraph = document.getElementById("first");
-paragraph.style.color = "white";
-var div = document.getElementById("second");
-div.style.color= "green";
+var colorList = ["DeepPink", "Salmon", "Orange", "Moccasin", "Gold",
+"Olive", "Aqua", "Lime", "Magenta", "Indigo", "Orchid", "SlateBlue",
+"MidnightBlue", "SkyBlue", "AntiqueWhite", "Tomato", "Lavendar"];
+var last = 35;
+for (i = 0; i< last; i++){
+  var cBox = document.createElement("DIV");
+  cBox.id = i;
+  cBox.className = "color-box";
+  cBox.innerHTML = 'test';
+  cBox.style.backgroundColor = colorList[i%17];
+  document.body.appendChild(cBox);
+  console.log('got to for loop');
+}
